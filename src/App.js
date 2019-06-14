ed2d2994d15e1faede9b2984334aaa614d77aaa5
Owen Ekhator <owenekhator@rocketmail.com>
2019-06-14 00:56:36 -0500

    added bootstrap link

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 
-import Homepage from './components/Homepage'
+import Homepage from './components/Homepage';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <Homepage/>
+      <Homepage />
     </div>
   );
 }
