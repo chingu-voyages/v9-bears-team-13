09f890e2e783d9384c83b1685c0d53746979da8f
Owen Ekhator <owenekhator@rocketmail.com>
2019-06-16 03:03:48 -0500

    modified nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 
 import Homepage from './components/homepage/Homepage';
 
--- src/components/homepage/nav/Nav.js
+++ src/components/homepage/nav/Nav.js
@@ -1,22 +1,50 @@
 import React from 'react';
 
+import './nav.css';
+
 const Nav = () => {
   return (
-    <div>
-      <ul className="nav justify-content-end">
-        <li className="nav-item">
-          <a className="nav-link active" href="#">
-            Login
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-            Signup
-          </a>
-        </li>
-      </ul>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar
+      </a>
+      
+
+    
+        <div className="collapse navbar-collapse " id="navbarSupportedContent">
+
+          <ul style={{width: '100%'}} className="navbar-nav mr-auto d-flex justify-content-end">
+            <li className="nav-item ">
+              <a className="nav-link" href="#">
+                <button type="button" className="btn btn-primary">
+                  Login
+                </button>
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                <button type="button" class="btn btn-danger">
+                  Signup
+                </button>
+              </a>
+            </li>
+          </ul>
+        </div>
+      
+    </nav>
   );
 };
 
-export default Nav
+export default Nav;
+
+// <div>
+//   <ul className="nav justify-content-end">
+//     <li className="nav-item">
+//       <button type="button" className="btn btn-primary">Login</button>
+//     </li>
+//     <li className="nav-item">
+//       <button type="button" class="btn btn-danger">Signup</button>
+//     </li>
+//   </ul>
+// </div>
