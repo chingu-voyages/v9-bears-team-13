05c6a3f47fd2f25bc85f0dc47f0767ef3a15b5bb
Owen Ekhator <owenekhator@rocketmail.com>
2019-06-17 03:06:36 -0500

    added continainer to homepage

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-import Nav from './nav/Nav'
+import Nav from './nav/Nav';
 
- const Homepage = () => {
+import './Homepage.css'
+
+const Homepage = () => {
   return (
-    
     <div>
-    <Nav/>
-      <h3>This is the Homepage</h3>
+      <Nav />
+      <div className="container">
+        <h3>This is the Homepage</h3>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
