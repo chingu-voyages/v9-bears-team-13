62e9828c87456d6878ba763e31f2ecb6080174a1
Andrew Horn <arhorn@smcm.edu>
2019-06-18 00:30:08 -0500

    homepage renders as main page instead of addWordPage

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import React from 'react';
 import Homepage from './components/homepage/Homepage';
 
 import './App.css';
-import AddWordPage from './components/addWordPage/AddWordPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <AddWordPage />
+      <Homepage />
     </div>
   );
 }
