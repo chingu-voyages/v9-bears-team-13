fd4cb5743379dbb68c6b1e729129f9c17f8e7dde
Owen Ekhator <owenekhator@rocketmail.com>
2019-06-19 18:38:01 -0500

    moved nav to app file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import Homepage from './components/homepage/Homepage';
-
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from './components/nav/Nav'
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Homepage />
     </div>
   );
