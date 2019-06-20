a42277a3d4c8fee6b5e86fb5f0f1e556afb661ca
Andrew Horn <arhorn@smcm.edu>
2019-06-20 16:08:57 -0500

    added Link to WordListPage

--- frontend/src/components/addWordPage/AddWordPage.js
+++ frontend/src/components/addWordPage/AddWordPage.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import Spaces from "./spaces/Spaces";
 import AddWordForm from "./addWordForm/AddWordForm";
 
 const AddWordPage = props => (
   <>
+    <br />
+    <br />
+    <Link to="/word-list">See word list</Link>
+    <br />
     <Spaces />
     <AddWordForm />
   </>
