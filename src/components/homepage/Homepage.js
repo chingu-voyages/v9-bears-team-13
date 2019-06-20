1b86930cafe93199f7059e8acb1478b3a97d2e9b
Owen Ekhator <owenekhator@rocketmail.com>
2019-06-19 19:24:05 -0500

    modified styles and removed unused code

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import Nav from './nav/Nav';
+
 
 import './Homepage.css'
 
 const Homepage = () => {
   return (
     <div>
-      <Nav />
+     
       <div className="container">
         <h3>This is the Homepage</h3>
       </div>
