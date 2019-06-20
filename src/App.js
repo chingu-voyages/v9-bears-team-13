4a6abe07e6ec425155dd41d036ff9a026d7f1cf8
Owen Ekhator <owenekhator@rocketmail.com>
2019-06-19 19:23:37 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import React from 'react';
 import Homepage from './components/homepage/Homepage';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from './components/nav/Nav'
+import Nav from './components/nav/Nav';
+
+import {Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Homepage />
+      //you would render each route like below. All you have to do is first import the component, set the path you want e.g '/home' or '/addword' and pass the component as a prop.
+      //all routes go below the Nav
+      <Route exact path="/" component={Homepage} />
     </div>
   );
 }
