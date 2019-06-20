2553fa86738c6047074107842a2377082b75c2bf
Andrew Horn <arhorn@smcm.edu>
2019-06-19 23:23:22 -0500

    deleted AddWordPage Nav component in favor of Nav in Router

--- src/components/addWordPage/AddWordPage.js
+++ src/components/addWordPage/AddWordPage.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
 
-import Nav from './nav/Nav';
 import Spaces from './spaces/Spaces';
 import AddWordForm from './addWordForm/AddWordForm';
 
 const AddWordPage = props => (
   <>
-    <Nav />
     <Spaces />
     <AddWordForm />
   </>
