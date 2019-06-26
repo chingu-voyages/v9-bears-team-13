57817329cd8b27b85eaf3c5423f3af36af1e0e66
Owen Ekhator <owenekhator@rocketmail.com>
2019-06-26 02:53:01 -0500

    provided rest props to component

--- frontend/src/helpers/privateRoute.js
+++ frontend/src/helpers/privateRoute.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 
-export const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={props =>
-      localStorage.getItem("authToken") ? (
-        <Component {...props} />
-      ) : (
-        <Redirect
-          to={{
-            pathname: "/",
-            state: { from: props.location }
-          }}
-        />
-      )
-    }
-  />
-);
+export const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        localStorage.getItem("authToken") ? (
+          <Component {...props} {...rest} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+};
