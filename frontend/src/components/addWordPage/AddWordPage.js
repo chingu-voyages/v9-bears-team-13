c39ac7ad2701bcb2a68895fbfbb9b541b238e3bf
Owen Ekhator <owenekhator@rocketmail.com>
2019-06-26 03:16:09 -0500

    added a useEffect method

--- frontend/src/components/addWordPage/AddWordPage.js
+++ frontend/src/components/addWordPage/AddWordPage.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import Spaces from "./spaces/Spaces";
 import AddWordForm from "./addWordForm/AddWordForm";
 
-const AddWordPage = props => (
-  <>
-    <br />
-    <br />
-    <Link to="/word-list">See word list</Link>
-    <br />
-    <Spaces />
-    <AddWordForm />
-  </>
-);
+const AddWordPage = props => {
+  useEffect(() => {
+    //props.getUser()
+    props.getUser();
+    // console.log(value)
+  }, []);
+
+  return (
+    <>
+      <br />
+      <br />
+      <Link to="/word-list">See word list</Link>
+      <br />
+      <Spaces />
+      <AddWordForm />
+    </>
+  );
+};
 
 export default AddWordPage;
