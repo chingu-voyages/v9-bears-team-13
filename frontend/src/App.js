afb66aa3cf4d3326d6093d92d58422b928988bd2
Owen Ekhator <owenekhator@rocketmail.com>
2019-06-26 03:00:41 -0500

    added getUser method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route } from "react-router-dom";
 
 import Homepage from "./components/homepage/Homepage";
@@ -10,16 +10,32 @@ import { PrivateRoute } from "./helpers/privateRoute";
 import AddWordPage from "./components/addWordPage/AddWordPage";
 import WordListPage from "./components/wordListPage/WordListPage";
 
+import axios from "axios";
+
+export const NameContext = React.createContext();
+
 function App() {
+  const [name, updateName] = useState("");
+
+  const getUser = () => {
+    axios
+      .get(
+        "https://bears-api.andrew-horn-portfolio.life/api/v1/rest-auth/user/"
+      )
+      .then(resp => console.log(resp.data))
+      .catch(err => console.log(err));
+  };
+
   return (
     <div className="App">
-      <Nav />
-      {/* you would render each route like below. All you have to 
-          do is first import the component, set the path you want 
-          e.g '/home' or '/addword' and pass the component as a 
-          prop.   all routes go below the Nav*/}
+      <Nav name={name} />
       <Route exact path="/" component={Homepage} />
-      <PrivateRoute path="/add-word" component={AddWordPage} />
+      <PrivateRoute
+        path="/add-word"
+        component={AddWordPage}
+        getUser={getUser}
+        name={name}
+      />
       <PrivateRoute path="/word-list" component={WordListPage} />
     </div>
   );
