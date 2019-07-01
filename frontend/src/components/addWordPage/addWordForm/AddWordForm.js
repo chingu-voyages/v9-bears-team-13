526b92272cd69bc1885763d89e01ae973b0211f9
Owen Ekhator <owenekhator@rocketmail.com>
2019-07-01 15:44:34 -0500

    added functionality to AddWordForm

--- frontend/src/components/addWordPage/addWordForm/AddWordForm.js
+++ frontend/src/components/addWordPage/addWordForm/AddWordForm.js
@@ -2,12 +2,54 @@ import React, { useState } from "react";
 import { Col, Form, Input, Button } from "reactstrap";
 import { withRouter } from "react-router";
 
+import axios from "axios";
+
 const AddWordForm = props => {
   const [word, setWord] = useState("");
 
   const onSubmitWord = e => {
     e.preventDefault();
-
+    if (props.words.map(element => element.word).includes(word)) {
+      let result = props.words.filter(element => element.word === word);
+      result = result[0];
+      axios
+        .put(
+          `https://bears-api.andrew-horn-portfolio.life/api/v1/${result.id}/`,
+          {
+            author: result.author,
+            word: result.word,
+            times_seen: result.times_seen + 1
+          },
+          {
+            headers: {
+              Authorization: `Token  ${localStorage.getItem("authToken")}`
+            }
+          }
+        )
+        .then(resp => {
+          props.getWords();
+        })
+        .catch(err => console.log(err));
+    } else {
+      axios
+        .post(
+          `https://bears-api.andrew-horn-portfolio.life/api/v1/`,
+          {
+            author: props.pkValue,
+            word,
+            times_seen: 1
+          },
+          {
+            headers: {
+              Authorization: `Token  ${localStorage.getItem("authToken")}`
+            }
+          }
+        )
+        .then(resp => {
+          props.getWords();
+        })
+        .catch(err => console.log(err));
+    }
     setWord("");
   };
 
