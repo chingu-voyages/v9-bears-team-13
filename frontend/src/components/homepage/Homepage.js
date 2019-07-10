aba11ed8b811ff84f5082beb54a5eac504f44a3f
Owen Ekhator <owenekhator@rocketmail.com>
2019-07-10 17:59:25 -0500

    added image source to homepage

--- frontend/src/components/homepage/Homepage.js
+++ frontend/src/components/homepage/Homepage.js
@@ -6,7 +6,13 @@ const Homepage = () => {
   return (
     <div>
       <div className="container">
-        <h3>This is the Homepage</h3>
+        <div className="wordlist_homepage">
+          <h3>Welcome</h3>
+          <img
+            src="https://www.heritagechinese.com/static/common/images/lipingchinese-logo.png"
+            alt="chinese word"
+          />
+        </div>
       </div>
     </div>
   );
