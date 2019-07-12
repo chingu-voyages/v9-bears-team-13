992ab3ebb7bbdcaee11cc4231d799a51d72e1c9f
Owen Ekhator <owenekhator@rocketmail.com>
2019-07-11 20:01:45 -0500

    changed font

--- frontend/src/components/homepage/Homepage.js
+++ frontend/src/components/homepage/Homepage.js
@@ -7,7 +7,7 @@ const Homepage = () => {
     <div className="full_height">
       <div className="container image-center">
         <div className="wordlist_homepage">
-          <h3>Welcome</h3>
+          <h3 className="header_style">Welcome</h3>
           <img
             src="https://www.heritagechinese.com/static/common/images/lipingchinese-logo.png"
             alt="chinese word"
